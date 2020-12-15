fef32a92d46013fe59d71c9466d4d7f298647a4e
Robert91911 <robert.marian9191@gmail.com>
2020-12-15 17:06:23 +0100

    Delete, ajusteLayout & validacion tabla clientes
    13. Escribiendo el delete en la clase service y en el componente clientes.mp4
    14. Overflow en listado de clientes, ajustando layout.mp4
    15. Validando los clientes en la tabla HTML con directiva ngIf.mp4

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { Cliente } from '../interfaces/Cliente';
 import { ClienteService } from './cliente.service';
 
@@ -17,6 +18,40 @@ export class ClientesComponent implements OnInit {
     this.clienteSrv.getClientes().subscribe(
       clientes => this.clientes = clientes
     )
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: '¿Estás seguro?',
+      text: `¿Estas seguro de que deseas eliminar el cliente ${cliente.nombre} ${cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminalo!',
+      cancelButtonText: 'No, cancela!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteSrv.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              'Cliente eliminado!',
+              `Cliente ${cliente.nombre} eliminado con éxito.`,
+              'success'
+            )
+          }
+        )
+
+      }
+    })
   }
 
 }
