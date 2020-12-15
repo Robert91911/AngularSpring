04fbb7fbdaeba5ff149298b125812b0b16e54f68
Robert91911 <robert.marian9191@gmail.com>
2020-12-15 17:26:57 +0100

    Manejo erores backend (getById)
    1. Manejo de error en el Backend en método handler show (obtener por id).mp4

--- API/spring-boot-backend-apirest/src/main/java/com/atos/springboot/backend/apirest/controllers/ClienteRestController.java
+++ API/spring-boot-backend-apirest/src/main/java/com/atos/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,12 +1,16 @@
 package com.atos.springboot.backend.apirest.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.atos.springboot.backend.apirest.models.entity.Cliente;
 import com.atos.springboot.backend.apirest.models.services.IClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,8 +38,25 @@ public class ClienteRestController {
 
     @GetMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.OK) //No hace falta ya que por defecto ya es un Ok
-    public Cliente show(@PathVariable Long id) {
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException e) {
+            response.put("Mensaje", "Error  al realizar la consulta a la base de datos");
+            response.put("error", e.getMessage().concat(": " ).concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(cliente == null) {
+            response.put("Mensaje", "El cliente ID: ".concat(id.toString().concat(" no existe en la base de datos")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+        
     }
 
     @PostMapping("/clientes")
