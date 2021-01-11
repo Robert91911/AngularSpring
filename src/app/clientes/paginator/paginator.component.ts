99ca17896200599cb173167781ee2ba8c209c037
Robert91911 <robert.marian9191@gmail.com>
2021-01-11 13:37:53 +0100

    Paginación desde el Backend P2
    8. Implementando los links de las páginas.mp4
    9. Implementando botones siguiente y atrás.mp4
    12. Añadiendo campo fecha en el formulario
    11. Paginación desde el Backend
    10. Implementando links para la primera y última página.mp4
    11. Paginador con muchas páginas overflow.mp4
    12. Implementando rangos de páginas en el componente Paginador.mp4
    13. Afinando más el evento ngOnChanges para refrescar los rangos en el paginador.mp4

--- src/app/clientes/paginator/paginator.component.ts
+++ src/app/clientes/paginator/paginator.component.ts
@@ -1,20 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.css']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
 
   @Input() paginador: any;
 
   paginas: number[];
 
+  desde: number;
+  hasta: number;
+
   constructor() { }
+  ngOnChanges( changes: SimpleChanges ): void {
+    let paginadorActualizado = changes['paginador'];
+
+    if(paginadorActualizado.previousValue) {
+      this.initPaginator();
+    }
+  }
 
   ngOnInit() {
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1) //_valor es para que no moleste el warning, es opcional
+    this.initPaginator();
+  }
+  
+  private initPaginator(): void {
+    this.desde = Math.min( Math.max( 1, this.paginador.number-4 ), this.paginador.totalPages-5 );
+    this.hasta = Math.max( Math.min( this.paginador.totalPages, this.paginador.number+4 ), 6 );
+
+    if( this.paginador.totalPages > 5 ) {
+
+      this.paginas = new Array(this.hasta - this.desde +1 ).fill(0).map((_valor, indice) => indice + this.desde) //_valor es para que no moleste el warning, es opcional
+
+    } else {
+
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1) //_valor es para que no moleste el warning, es opcional
+
+    }
   }
 
 }
